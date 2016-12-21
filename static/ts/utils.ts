96c98b2fc2e531f3fc99597e64448f6f9bbdc21c
Davide Canton <davide.canton5@gmail.com>
2016-12-21 23:14:29 +0100

    switch to ts

---
+++ static/ts/utils.ts
@@ -0,0 +1,21 @@
+/// <reference path="../typings/index.d.ts" />
+import * as ko from 'knockout';
+
+export class Utils {
+    static fileSize(a : number) : string
+    {
+        var e = Math.log(a) / Math.log(1024) | 0;
+        return (a / Math.pow(1024, e)).toFixed(2)
+            + ' ' + (e ? 'KMGTPEZY'[--e] + 'iB' : 'Bytes');
+    }
+
+    static push_and_remove<T> (array : ko.MaybeObservableArray<T>, data: T, plot_size : number)
+    {
+        array = ko.unwrap(array);
+
+        array.push(data);
+        if (array.length > plot_size)
+            array.shift();        
+    }
+
+};
